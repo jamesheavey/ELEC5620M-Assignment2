a44c8ff902c452eb908f1af7e85e7dfe94b6afa7
James Heavey <james@heavey.net>
2021-03-17 19:18:37 +0000

    Designed basic MM:SS:FF timer
    Timer displays on the 7 segment displays, however time period appears to be too small, each measurement is running faster than it should

--- 2-A-StopWatch/main.c
+++ 2-A-StopWatch/main.c
@@ -7,11 +7,44 @@
 
 //Include ResetWDT() macro:
 #include "HPS_Watchdog/HPS_Watchdog.h"
+#include "HPS_PrivateTimer/HPS_PrivateTimer.h"
+#include "DE1SoC_SevenSeg/DE1SoC_SevenSeg.h"
+
+volatile unsigned int *key_ptr = (unsigned int *)0xFF200050;  // key buttons base address
 
 //Main Function
 int main(void) {
-    /* Main Run Loop */
-    while(1) {
-        HPS_ResetWatchdog();
-    }
+    /* Local Variables */
+	unsigned int lastBlinkTimerValue[4] = {0};
+	unsigned int time_vals[4] = {0};
+	const unsigned int blinkPeriod[4] = {500000,50000000,3000000000,180000000000};
+
+	Timer_initialise(0xFFFEC600);
+	Timer_setLoad(0xFFFFFFFF);
+	Timer_setPrescaler(0);
+	Timer_setControl(0, 1, 1);
+
+	/* Main Run Loop */
+	while(1) {
+		// Read the current time
+		unsigned int currentTimerValue = Timer_readCurrentValue();
+		int i;
+
+		for (i = 0; i < 4; i++) {
+			if ((lastBlinkTimerValue[i] - currentTimerValue) >= blinkPeriod[i]) {
+
+				time_vals[i] =  time_vals[i] + 1;
+
+				lastBlinkTimerValue[i] = lastBlinkTimerValue[i] - blinkPeriod[i];
+			}
+		}
+
+		DE1SoC_SevenSeg_SetDoubleDec(0,time_vals[0]%99);
+		DE1SoC_SevenSeg_SetDoubleDec(2,time_vals[1]%60);
+		DE1SoC_SevenSeg_SetDoubleDec(4,time_vals[2]%60);
+
+		Timer_clearInterruptFlag();
+		// Finally, reset the watchdog timer.
+		HPS_ResetWatchdog();
+	}
 }
