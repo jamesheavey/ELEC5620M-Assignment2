317048359287d5c9deeef946aa6349ca0a4d9316
James Heavey <james@heavey.net>
2021-03-17 23:07:10 +0000

    Added Split functionality
    Added a ability to 'split' by pressing the second button. This converts the current time to seconds and displays it on the LEDs.

--- 2-A-StopWatch/main.c
+++ 2-A-StopWatch/main.c
@@ -5,24 +5,39 @@
  *      Author: James
  */
 
-//Include ResetWDT() macro:
 #include "HPS_Watchdog/HPS_Watchdog.h"
 #include "HPS_PrivateTimer/HPS_PrivateTimer.h"
 #include "DE1SoC_SevenSeg/DE1SoC_SevenSeg.h"
 
 volatile unsigned int *key_ptr = (unsigned int *)0xFF200050;  // key buttons base address
+volatile unsigned int *LED_ptr = (unsigned int *) 0xFF200000;
+
 const unsigned int scaler = 200 - 1;
 const unsigned int period = 225000000/(scaler+1); 			  // 225MHz
 
-const unsigned int RESET[4] = {0};
+const unsigned int TIMER_SIZE = 4;
+const unsigned int RESET[TIMER_SIZE] = {0};
+const unsigned int COEFFICIENTS[TIMER_SIZE] = {1/100, 1, 60, 3600};
 
+
+unsigned int timer_to_seconds(unsigned int time[]){
+
+	unsigned int total_time = 0;
+
+	int i;
+	for (i = 1; i < TIMER_SIZE; ++i) {
+		total_time += time[i] * COEFFICIENTS[i];
+	}
+
+	return total_time;
+}
 
 //Main Function
 int main(void) {
     /* Local Variables */
-	unsigned int lastIncrementTimerValue[4] = {0};
-	unsigned int time_vals[4] = {0};
-	const unsigned int incrementPeriod[4] = {period/100,period,period*60,period*60*60};
+	unsigned int lastIncrementTimerValue[TIMER_SIZE] = {0};
+	unsigned int time_vals[TIMER_SIZE] = {0};
+	const unsigned int incrementPeriod[TIMER_SIZE] = {period/100,period,period*60,period*3600};
 
 	int hour_trigger = 0;
 	bool first = true;
@@ -33,11 +48,14 @@ int main(void) {
 
 	/* Main Run Loop */
 	while(1) {
+		int i;
 
 		if (*key_ptr && 0x1) { memcpy(time_vals, RESET, sizeof(RESET)); }
 
 
-		for (int i = 0; i < 4; i++) {
+		if (*key_ptr && 0x2) { *LED_ptr = timer_to_seconds(time_vals); }
+
+		for (i = 0; i < TIMER_SIZE; i++) {
 			if ((lastIncrementTimerValue[i] - Timer_readCurrentValue()) >= incrementPeriod[i]) {
 
 				time_vals[i] =  time_vals[i] + 1;
@@ -55,4 +73,3 @@ int main(void) {
 		HPS_ResetWatchdog();
 	}
 }
-
