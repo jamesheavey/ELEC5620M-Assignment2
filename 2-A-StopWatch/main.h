0fc8ad51be843be29330303a06eb60e7ab07b678
James Heavey <james@heavey.net>
2021-03-19 16:20:59 +0000

    Created A task Scheduler
    Task scheduler now handles individual unit increments.

--- 2-A-StopWatch/main.h
+++ 2-A-StopWatch/main.h
@@ -16,21 +16,26 @@ const unsigned int period = 225000000/(scaler+1); 			  // 225MHz
 
 const unsigned int TIMER_SIZE = 4;
 const unsigned int LED_MAX = 1024;                            // 2^10 as there are 10 LEDs
-const unsigned int COEFFICIENTS[TIMER_SIZE] = {100,60,60,24};
 
-void init_timer() {
+typedef void (*TaskFunction)(unsigned int*);
+
+
+void init_timer()
+{
 	Timer_initialise(0xFFFEC600);
 	Timer_setLoad(0xFFFFFFFF);
 	Timer_setControl(scaler, 0, 1, 1);
 }
 
-void init_lcd() {
+void init_lcd()
+{
 	LT24_initialise(0xFF200060,0xFF200080);
 	HPS_ResetWatchdog();
 	LT24_clearDisplay(LT24_BLACK);
 }
 
-void intro() {
+void intro()
+{
 	const int num_chars = 27;
 	unsigned int intro_message[num_chars] = {0xF,0xF,0x9,0xA,0xF,0xB,0xC,0xD,0xE,0x6,0x8,0xF,0xF,0xF,0x0,0x1,0x2,0xF,0x3,0x4,0xF,0x5,0x6,0x6,0x7,0x8,0xF};
 	unsigned int lastIncrTime = 0;
@@ -63,7 +68,8 @@ void intro() {
 	Timer_setLoad(0xFFFFFFFF);
 }
 
-void draw_time(unsigned int timeValues[], int x, int y, int scale){
+void draw_time(unsigned int timeValues[], int x, int y, int scale)
+{
 	LT24_drawCharDoubleDec(timeValues[3], LT24_WHITE, x , y, 5, 8, scale);
 	LT24_drawChar(BF_fontMap[26], LT24_WHITE, x +1.5*(2*5*scale), y, 5, 8, scale);
 	LT24_drawCharDoubleDec(timeValues[2], LT24_WHITE, x +2*(2*5*scale), y, 5, 8, scale);
@@ -73,14 +79,16 @@ void draw_time(unsigned int timeValues[], int x, int y, int scale){
 	LT24_drawCharDoubleDec(timeValues[0], LT24_WHITE, x +6*(2*5*scale), (y+8*scale/2), 5, 8, scale/2);
 }
 
-void update_lcd(unsigned int timeValues[]){
+void update_lcd(unsigned int timeValues[])
+{
 	// split each into 2 parts
 	LT24_clearDisplay(LT24_BLACK);
 
 	draw_time(timeValues, 20, 20, 3);
 }
 
-unsigned int timer_to_LEDs(unsigned int time[]){
+unsigned int timer_to_LEDs(unsigned int time[])
+{
 
 	if(time[1] < LED_MAX){
 		return time[1];
@@ -91,14 +99,16 @@ unsigned int timer_to_LEDs(unsigned int time[]){
 	}
 }
 
-void draw_split(unsigned int timeValues[], int x, int y, int scale, int splitNum){
+void draw_split(unsigned int timeValues[], int x, int y, int scale, int splitNum)
+{
 
 	LT24_drawChar(BF_fontMap[16 + splitNum%10], LT24_WHITE, x-(5*scale)-10, y, 5, 8, scale);
 	LT24_drawChar(BF_fontMap[26], LT24_WHITE, x-(5*scale), y, 5, 8, scale);
 	draw_time(timeValues, x, y, scale);
 }
 
-void pause(){
+void pause()
+{
 	Timer_setControl(scaler, 0, 1, 0);
 
 	while (*key_ptr & 0x4) {HPS_ResetWatchdog();};
@@ -108,22 +118,45 @@ void pause(){
 	Timer_setControl(scaler, 0, 1, 1);
 }
 
-void split(unsigned int timeValues[], int *splitNum){
+void split(unsigned int timeValues[], int *splitNum)
+{
 	*splitNum++;
 	*LED_ptr = timer_to_LEDs(timeValues);
 	draw_split(timeValues, 40, 40*(*splitNum), 2, *splitNum);
 	while (*key_ptr & 0x2) {HPS_ResetWatchdog();};
 }
 
-void mode_toggle(bool* mode){
+void mode_toggle(bool* mode)
+{
 	while (*key_ptr & 0x8) {HPS_ResetWatchdog();};
 	*mode = !(*mode);
+}
+
+void hundredths(unsigned int* timeValue)
+{
+	*timeValue = (*timeValue +1)% 100;
+}
+
+void seconds(unsigned int* timeValue)
+{
+	*timeValue = (*timeValue +1)% 60;
+}
+
+void minutes(unsigned int* timeValue)
+{
+	*timeValue = (*timeValue +1)% 60;
+}
+
+void hours(unsigned int* timeValue)
+{
+	*timeValue = (*timeValue +1)% 24;
 }
 
 void timer() {
 	unsigned int lastIncrTime[TIMER_SIZE] = {0};
 	unsigned int timeValues[TIMER_SIZE] = {0};
 	const unsigned int incrPeriod[TIMER_SIZE] = {period/100,period,period*60,period*3600};
+	TaskFunction taskFunctions[TIMER_SIZE] = {&hundredths,&seconds,&minutes,&hours};
 	bool mode = false;
 	int splitNum = 0;
 
@@ -149,7 +182,9 @@ void timer() {
 
 		for (i = 0; i < TIMER_SIZE; i++) {
 			if ((lastIncrTime[i] - Timer_readValue()) >= incrPeriod[i]) {
-				timeValues[i] =  (timeValues[i] + 1) % COEFFICIENTS[i];
+
+				taskFunctions[i](&timeValues[i]);
+
 				lastIncrTime[i] = lastIncrTime[i] - incrPeriod[i];
 
 				DE1SoC_SevenSeg_SetDoubleDec(0,timeValues[0+mode]);
